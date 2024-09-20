526a2b76fa8140f46affafe4757d5a5e8c7a3859
KalapalaGIT <lassi.uk@gmail.com>
2024-09-20 14:19:14 +0300

    Making cards

---
+++ Cards.py
@@ -0,0 +1,38 @@
+import random
+
+# Define card suits and ranks
+SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+    def get_value(self):
+        if self.rank in ['J', 'Q', 'K']:
+            return 10
+        elif self.rank == 'A':
+            return 11
+        else:
+            return int(self.rank)
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(suit, rank) for suit in SUITS for rank in RANKS]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def print_deck(self):
+        for card in self.cards:
+            print(card)
+
+    def draw(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        else:
+            return None
---
+++ main.py
@@ -0,0 +1,9 @@
+from Cards import Deck
+
+deck = Deck()
+
+deck.shuffle()
+deck.print_deck()
+print("")
+deck.__init__()
+deck.print_deck()
